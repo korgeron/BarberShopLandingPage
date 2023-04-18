eeb94d98c34b871332aae92f93995af21a2523c7
kevin <orgeron.kevin@gmail.com>
2023-04-17 20:01:00 -0500

    working on mobile / desktop navbars

--- scripts.js
+++ scripts.js
@@ -2,6 +2,19 @@
 
 const navbar = document.querySelector('.navbar');
 const navLinks = document.querySelector('.nav-links');
+const barBtn = document.querySelector('.bar-btn');
+const xBtn = document.querySelector('.x-btn');
 
+barBtn.addEventListener('click', ()=>{
+    barBtn.style.display = 'none';
+    xBtn.style.display = 'block';
+    navbar.style.height = '30vh';
+})
+
+xBtn.addEventListener('click', ()=>{
+    barBtn.style.display = 'block';
+    xBtn.style.display = 'none';
+    navbar.style.height = '0';
+})
 
 // END OF MOBILE
